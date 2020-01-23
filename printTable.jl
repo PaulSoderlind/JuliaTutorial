4a939aa7f0b8007bac0213d90b0fb5d353af684c
Soderlind <Paul.Soderlind@unisg.ch>
2020-01-23 10:47:17 +0100

    allow header in cell(0,0)

--- printTable.jl
+++ printTable.jl
@@ -1,14 +1,34 @@
 """
-    printTable([fh::IO],x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=false,htmlQ=false)
+    printTable([fh::IO],x,colNames=[],rowNames=[];
+               width=10,prec=3,NoPrinting=false,htmlQ=false,cell00="")
 
-Print formatted row names (1st column) column names (1st row), and data matrix (the rest).
+Print formatted table with row names (1st column) column names (1st row), and data matrix (the rest).
 
-See printmat() for (width,prec)
 
-To do: (a) allow for different n-vectors width&prec
+# Input
+- `fh::IO`:           (optional) file handle. If not supplied, prints to screen
+- `x::Array`:         string, date or array to print
+- `width::Int`:       (keyword) scalar, width of printed cells. [10]
+- `prec::Int`:        (keyword) scalar, precision of printed cells. [3]
+- `NoPrinting::Bool`: (keyword) bool, true: no printing, just return formatted string [false]
+- `hmtlQ::Bool`:      (keyword) bool, true: format as htmlQ <td>cells</td> [false]
+- `cell00::String`:   (keyword) string, for row 0, column 0
+
+# Output
+- `str::String`:      (if NoPrinting) string, (otherwise nothing)
+
+# Example
+```
+xA = [1 "ab" "abc"; "ccc" 3.14 missing]
+printTable(xA,colNames,["1";"4"],width=12,prec=2)
+```
+
+# Uses
+- printmat
 
 """
-function printTable(fh::IO,x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=false,htmlQ=false)
+function printTable(fh::IO,x,colNames=[],rowNames=[];
+                    width=10,prec=3,NoPrinting=false,htmlQ=false,cell00="")
 
   isempty(x) && return nothing                        #do nothing is isempty(x)
 
@@ -22,15 +42,16 @@ function printTable(fh::IO,x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=
   end
 
   rNamesWidth = maximum([length(rowNames[i]) for i = 1:length(rowNames)])  #max length of rowNames
+  rNamesWidth = max(rNamesWidth,length(cell00))
 
   if htmlQ                                             #print column names
-    cNamesStr = string("<tr><td>",rpad("",rNamesWidth),"</td>")
+    cNamesStr = string("<tr><th>",lpad(cell00,rNamesWidth),"</th>")
     for i = 1:n
       cNamesStr = string(cNamesStr,"<th>",lpad(colNames[i],width),"</th>")
     end
     cNamesStr = string(cNamesStr,"</tr>")
   else
-    cNamesStr = rpad("",rNamesWidth)                     #"" for cell 0,0
+    cNamesStr = lpad(cell00,rNamesWidth)                 #cell 0,0
     for i = 1:n                                          #create string
       cNamesStr = string(cNamesStr,lpad(colNames[i],width))
     end
@@ -59,9 +80,9 @@ function printTable(fh::IO,x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=
 
 end
                         #when fh is not supplied: printing to screen
-printTable(x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=false,htmlQ=false) =
+printTable(x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=false,htmlQ=false,cell00="") =
 printTable(stdout::IO,x,colNames,rowNames,
-                      width=width,prec=prec,NoPrinting=NoPrinting,htmlQ=htmlQ)
+                      width=width,prec=prec,NoPrinting=NoPrinting,htmlQ=htmlQ,cell00=cell00)
 #------------------------------------------------------------------------------
 
 
@@ -71,10 +92,10 @@ printTable(stdout::IO,x,colNames,rowNames,
 
 Call on printTable2 twice: to print to screen and then to an open file (IOStream)
 """
-function printTable2(fh,x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=false,htmlQ=false)
-  printTable(x,colNames,rowNames,width=width,prec=prec,NoPrinting=NoPrinting,htmlQ=htmlQ)      #to screen
+function printTable2(fh,x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=false,htmlQ=false,cell00="")
+  printTable(x,colNames,rowNames,width=width,prec=prec,NoPrinting=NoPrinting,htmlQ=htmlQ,cell00=cell00)      #to screen
   if isa(fh,IOStream) && isopen(fh)
-    printTable(fh,x,colNames,rowNames,width=width,prec=prec,NoPrinting=NoPrinting,htmlQ=htmlQ) #to file
+    printTable(fh,x,colNames,rowNames,width=width,prec=prec,NoPrinting=NoPrinting,htmlQ=htmlQ,cell00=cell00) #to file
   end
 end
 #------------------------------------------------------------------------------
