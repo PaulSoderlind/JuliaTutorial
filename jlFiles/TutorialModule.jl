854b11db53b7653b829be3a2a48e2015c27714b9
Paul Soderlind <Paul.Soederlind@gmail.com>
2023-06-15 09:57:59 +0200

    more comments on modules

--- jlFiles/TutorialModule.jl
+++ jlFiles/TutorialModule.jl
@@ -1,11 +1,11 @@
 module TutorialModule
 
-using Printf                       #the module can use other packages
+using Printf                                          #the module can use other packages
 #import LinearAlgebra
 
-export printmat, printlnPs, printblue, printTeXTable   #available after `using TutorialModule`
+export printmat, printlnPs, printblue, printTeXTable  #available after `using TutorialModule`
 
-include("printmat.jl")                                 #code
-include("printTeXTable.jl")
+include("printmat.jl")                                #files with the functions
+include("printTeXTable.jl")                           #could also type in the code here
 
 end
