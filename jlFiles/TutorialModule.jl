438c25e69f163721a08f19358ae4cb37b228406a
Paul Soderlind <Paul.Soederlind@gmail.com>
2023-11-08 17:22:51 +0100

    two ways to work with modules

--- jlFiles/TutorialModule.jl
+++ jlFiles/TutorialModule.jl
@@ -1,9 +1,9 @@
 module TutorialModule
 
-using Printf                                          #the module can use other packages
+import Printf                                         #the module can use other packages
 #import LinearAlgebra
 
-export printmat, printlnPs, printblue, printTeXTable  #available after `using TutorialModule`
+export printmat, printlnPs, printblue, printTeXTable  #available after `using .TutorialModule`
 
 include("printmat.jl")                                #files with the functions
 include("printTeXTable.jl")                           #could also type in the code here
