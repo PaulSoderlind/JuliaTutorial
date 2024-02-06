be17d1eb766fcb322e1f887aa2c8a5ae714b30a5
Paul Soderlind <Paul.Soederlind@gmail.com>
2024-02-06 20:56:46 +0100

    better coding of TeX table fn

--- jlFiles/printTeXTable.jl
+++ jlFiles/printTeXTable.jl
@@ -1,22 +1,23 @@
-#------------------------------------------------------------------------------
 """
-    printTeXTable(fh::IO,x;colNames=[],rowNames=[],width=12,prec=2) or
-    printTeXTable(x;colNames=[],rowNames=[],width=12,prec=2) or
+    printTeXTable([fh::IO,]x;colNames=[],rowNames=[],width=12,prec=2) or
 
 Create LaTeX table from numeric matrix x. Print to file or screen and returns a string
 with the table contents.
 
 # Input
--`fh::IOStream`:
--`x::`:
--`colNames::`:
--`rowNames::`:
--`width::`:
--`prec::`:
+-`fh::IOStream`:       handle of open file, optional
+-`x::VecOrMat`:        numbers for the table
+-`colNames::Vector`:   of strings, column names
+-`rowNames::Vector`:   of string, row names
+-`width::Int`:         width of numerical cells
+-`prec::Int`:          number of digits of numerical cells
+
+# Requires
+- fmtNumPs from the printmat.jl file
 
 
 """
-function printTeXTable(fh::IO,x;colNames=[],rowNames=[],width=12,prec=2)
+function printTeXTable(fh::IO,x;colNames=String[],rowNames=String[],width=12,prec=2)
 
   (m,n) = (size(x,1),size(x,2))
 
@@ -25,35 +26,22 @@ function printTeXTable(fh::IO,x;colNames=[],rowNames=[],width=12,prec=2)
 
   str = """
   \\begin{table}
-    \\begin{tabular}{l"""
-  str = string(str,"r"^n,"}\n","    ")
+    \\begin{tabular}{l$(repeat("r",n))}\n    """                    #beginning of table
 
-  str = string(str," & ")                              #empty cell above rowNames[1]
-  for i = 1:n-1                                        #column names
-    str = string(str,colNames[i]," & ")
-  end
-  str = string(str,colNames[n]," \\\\ \\hline \n")
-
-  for i = 1:m                                                          #loop over rows
-    str = string(str,"   ",rowNames[i]," & ")                          #row name
-    for j = 1:n-1
-      str = string(str,fmtNumPs(x[i,j],width,prec,"right"), " & ")     #x[i,1:end-1]
-    end
-    str = string(str,fmtNumPs(x[i,n],width,prec,"right")," \\\\ \n")   #x[i,end]
+  str = string(str," & ",join(colNames," & ")," \\\\ \\hline \n")   #row names and hline
+  for i = 1:m                                                       #loop over rows
+    str = string( str,"   $(rowNames[i]) & ",
+                  join(fmtNumPs.(x[i,:],width,prec,"right")," & ")," \\\\ \n" )
   end
 
-  endstr = """
-    \\hline
-    \\end{tabular}
-  \\end{table}"""
-  str = string(str,endstr,"\n")
+  str = string(str,"  \\hline\n  \\end{tabular}\n\\end{table}\n")   #end of table
 
-  print(fh,str)                                                        #print
+  print(fh,str)                                                     #print
 
   return str
 
 end
-                                      #when fh is not supplied: printing to screen
-printTeXTable(x;colNames=[],rowNames=[],width=12,prec=2) =
+
+                                       #when fh is not supplied: printing to screen
+printTeXTable(x;colNames=String[],rowNames=String[],width=12,prec=2) =
 printTeXTable(stdout::IO,x;colNames,rowNames,width,prec)
-#------------------------------------------------------------------------------
