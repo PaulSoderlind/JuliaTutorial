19d144c6b73b0a9743f25e297b3ec13048bea3fa
Paul Soderlind <Paul.Soederlind@gmail.com>
2026-08-21 13:02:37 +0200

    a bit more general code

--- src/printmat.jl
+++ src/printmat.jl
@@ -10,8 +10,8 @@ The `colNames` for Tables default to the `Tables.columnnames()`.
 # Input
 - `fh::IO`:            (optional) file handle. If not supplied, prints to screen
 - `x::Array(s) or Tables`:  (of numbers, dates, strings, ...) to print
-- `colNames::Array`:     of strings with column headers, alternatively like "c" to get "c1,c2,..."
-- `rowNames::Array`:     of strings with row labels, alternatively like "r" to get "r1,r2,..."
+- `colNames::Vector`:    (keyword) of strings with column headers, alternatively like "c" to get "c1,c2,..."
+- `rowNames::Vector`:    (keyword) of strings with row labels, alternatively like "r" to get "r1,r2,..."
 - `width::Int`:          (keyword) minimum width of printed cells
 - `prec::Int`:           (keyword) precision of printed cells (number of digits after .)
 - `NoPrinting::Bool`:    (keyword) if true: no printing, just return formatted string [false]
@@ -36,7 +36,8 @@ printmat([11,12],[21,22])
 ```
 Can also call as
 ```
-opt = Dict(:rowNames=>["1";"4"],:width=>10,:prec=>3,:NoPrinting=>false,:StringFmt=>"")
+opt = Dict(:rowNames=>["1";"4"],:width=>10,:prec=>3,:NoPrinting=>false,:StringFmt=>"") or
+opt = (rowNames=["1";"4"],width=10,prec=3,NoPrinting=false,StringFmt="")
 printmat(x;colNames=["a","b"],opt...)     #notice ; and ...
 ```
 (not all keywords are needed)
@@ -47,7 +48,7 @@ printmat(x;colNames=["a","b"],opt...)     #notice ; and ...
 
 # Notice
 - For Tables.table, the Tables.columnnames() are used for colNames (if they exist and colNames=[])
-- The code is not optimized for speed. I just aims to be fairly simple and get the job done.
+- The code is not optimized for speed. It just aims to be fairly simple and get the job done.
 
 Paul.Soderlind@unisg.ch
 
@@ -66,24 +67,23 @@ function printmat(fh::IO,x...;colNames=String[],rowNames=String[],
   end
 
   x     = hcatWithTypePs(x...)     #create matrix as in hcat(x...), but defaulting to Any if eltypes differ
-  (m,n) = (size(x,1),size(x,2))    #hcat(x...) also converts OffsetArrays to tradional arrays
+  (m,n) = (size(x,1),size(x,2))    #hcat(x...) also converts OffsetArrays to traditional arrays
 
-  colNames = ExpandrcNamesPs(colNames,n)            #(a) if non-empty, single string to Vector;
-  rowNames = ExpandrcNamesPs(rowNames,m)            #(b) if non-empty, to n-vector with numbers
+  colNames = _ExpandrcNamesPs(colNames,n)            #(a) if non-empty, single string to Vector;
+  rowNames = _ExpandrcNamesPs(rowNames,m)            #(b) if non-empty, to n-vector with numbers
 
-  cell00    = isempty(rowNames) ? "" : cell00                                 #force cell00="" if no rowNames
-  col0Width = isempty(rowNames) ? 0  : maximum(length,vcat(cell00,rowNames))  #width of column 0 (cell00 and rowNames)
-  colWidth  = fill(width,n)                                                   #widths of column 1:n
+  cell00    = all(isempty,rowNames) ? "" : cell00                                 #force cell00="" if no rowNames
+  col0Width = all(isempty,rowNames) ? 0  : maximum(length,vcat(cell00,rowNames))  #width of column 0 (cell00 and rowNames)
+  colWidth  = fill(width,n)                                                       #widths of column 1:n
 
   iob = IOBuffer()
 
-  if isa(colNames,Vector) && (length(colNames)==n)                            #if there are column names
+  if any(!isempty,colNames) && (length(colNames)==n)                            #if there are column names
     colNamesPrint(iob,colNames,n,cell00,col0Width,colWidth,delim)
     colUnderlineQ && colNamesPrint(iob,colNames,n,cell00,col0Width,colWidth,delim,true)
   end
 
   aLine = fill("",1+n)
-  isempty(rowNames) && (rowNames = fill("",m))        #empty rowNames, if needed
   for i = 1:m                                         #print each line: [rowName[i] x[i,:]]
     aLine .= ""                                       #reset the line to empty
     aLine[1] = rpad(rowNames[i],col0Width)
@@ -167,7 +167,7 @@ function fmtNumPs(z,width=10,prec=2,justify="right")
   isa(z,Bool) && (z = convert(Int,z))             #Bool -> Int
 
   if isa(z,AbstractFloat)                         #example: 101.0234, prec=3
-    fmt   = Printf.Format("%$(width).$(prec)f")
+    fmt   = Printf.Format("%.$(prec)f")           #notice: no width parameter
     strLR = Printf.format(fmt,z)
   elseif isa(z,Nothing)
     strLR = ""
@@ -176,28 +176,31 @@ function fmtNumPs(z,width=10,prec=2,justify="right")
   else                                            #Int, String, Date, Missing, etc
     strLR = string(z)
   end
-                                                #justification
-  strLR = justify == "left" ? rpad(strLR,width) : lpad(strLR,width)
+
+  if isfinite(width)                              #justification, width, but only if width != ∞
+    strLR = justify == "left" ? rpad(strLR,width) : lpad(strLR,width)
+  end
 
   return strLR
 
 end
-#---------------------------------------------------s---------------------------
+#-------------------------------------------------------------------------------
 
 
 ##------------------------------------------------------------------------------
 """
     hcatWithTypePs(x...)
 
-Does hcat(x...), but defaults to an `Any` matrix if the `eltype`s
+Does hcat(x...), but defaults to a type union matrix if the `eltype`s
 differ across `x[i]`. Otherwise, a standard `hcat(x...)`
 
 """
 function hcatWithTypePs(x...)
-  typeTestQ = any(!=(eltype(x[1])),eltype.(x))  #test if eltype(x[i]) differs
-  if typeTestQ                                      #create matrix from tuple created by x...
-    x = hcat(convert(Array{Any},hcat(x[1])),x[2:end]...)   #preserving types of x[i]
-  else                                                     #hcat(x[1]) to guarantee a matrix
+  Types = eltype.(x)
+  if !allequal(Types)
+    TypesU = Union{Types...}
+    x = hcat(convert(Array{TypesU},hcat(x[1])),x[2:end]...)  #preserving types of x[i]
+  else                                                       #hcat(x[1]) to guarantee a matrix
     x = hcat(x...)
   end
   return x
@@ -210,8 +213,8 @@ end
     table_to_matrix(tab)
 
 Help function to facilitate printing of a Tables.table. The function creates a matrix from
-a row or column table, and also extract the column names. If `tab` is an array, then it is
-just copied.
+a row or column table, and also extract the column names. If `tab` is an array, then
+it is outputted without change.
 
 """
 function table_to_matrix(tab)
@@ -229,18 +232,16 @@ end
 
 ##------------------------------------------------------------------------------
 """
-    ExpandrcNamesPs(rcNames,n)
+    _ExpandrcNamesPs(rcNames,n)
 
-Expand "abc" to ["abc"] (leaving ["def"] and "" untouched) and then also create
-a vector of ["def1","def2",...,"defn"] (or similarly for any non-empty string)
+Expand "" to ["","",etc] or "r" to ["r1","r2",etc]
 
 """
-function ExpandrcNamesPs(rcNames,n)
-  if isa(rcNames,String) && !isempty(rcNames)           #"abc" to ["abc"], but leave "" untouched
-    rcNames = [rcNames]
-  end
-  if length(rcNames) == 1 < n
-    rcNames = string.(only(rcNames),1:n)                #create "ci" from ["c"] for i=1:n
+function _ExpandrcNamesPs(rcNames,n)
+  if isempty(rcNames)                                #"" to ["","",etc]
+     rcNames = fill("",n)
+  elseif isa(rcNames,String) && !isempty(rcNames)    #"r" to ["r1","r2",etc]
+    rcNames = string.(only(rcNames),1:n)
   end
   return rcNames
 end
@@ -249,7 +250,9 @@ end
 
 ##------------------------------------------------------------------------------
 """
+    colNamesPrint
 
+Print column headers, or (if `DoUnderline=true`) underline existing column headers
 
 """
 function colNamesPrint(iob,colNames,n,cell00,col0Width,colWidth,delim="",DoUnderline=false)
@@ -287,16 +290,7 @@ end
 #------------------------------------------------------------------------------
 
 
-#------------------------------------------------------------------------------
-"""
-    doc2(fn)
 
-Macro to display the documentation of a function or other object.
-Call it as `@doc2 printmat`. For use in notebooks in VS code.
-(In most other cases `@doc printmat` works well.)
 
-"""
-macro doc2(fn)
-  return :( display("text/markdown",Base.doc($(esc(fn)))))
-end
-#------------------------------------------------------------------------------
+
+
